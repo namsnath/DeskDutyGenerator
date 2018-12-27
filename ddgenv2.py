ca0a6cb92d2170fc57228146265fd555b2490024
Namit Nathwani <namsnath@gmail.com>
2018-12-28 01:03:52 +0530

    Added calculateScore function for fitness according to free slots

--- ddgenv2.py
+++ ddgenv2.py
@@ -19,6 +19,7 @@ ______________________________________________________
 ################################################## Constants ##################################################
 
 slotsPerDay = 11
+dutySlotsPerDay = 8
 dayCount = 5
 buildings = 2
 dutiesPerBuilding = 2
@@ -32,8 +33,8 @@ core = ['Atul', 'Ayush', 'Charu', 'Kabiir', 'Krityaan', 'Manisha', 'Namit', 'Pav
 		'Aban', 'Aradhita', 'Bhavishya', 'Chinmay', 'David', 'Devam', 'Fardeen', 'Gauri', 'Kuhoo', 'Lakshay', 'Manan', 'Prajeeth', 'Rishit', 'Rohan', 'Ruchica', 
 		'Sanjana', 'SauravH', 'Siddharth', 'Suranjan', 'Taarussh', 'Tajendar', 'Tanya', 'Thiru', 'Vaishnavi']
 
-srData = open("srCoreData.txt")
-seniorCoreData = json.loads(srData.read())
+data = open("coreData.txt")
+coreData = json.loads(data.read())
 
 singleBreaks = {i: None for i in core}
 
@@ -43,13 +44,14 @@ maxSlots = {
 }
 
 points = {
+	"free": 1.0,
 	"total": 0.4,
 	"daily": 0.5,
-	"singleBreak": 2,
+	"singleBreak": 2.0,
 	"doubleBreak": 0.5,
-	"fullDuty": 20,
-	"venue": 1,
-	"empty": -20,
+	"fullDuty": 20.0,
+	"venue": 1.0,
+	"empty": -20.0,
 }
 
 
@@ -67,7 +69,7 @@ def getPerson():
 	return random.choice(core)
 
 def createChromosome():
-	length = dayCount * maxSlots["daily"] * buildings * dutiesPerBuilding
+	length = dayCount * dutySlotsPerDay * buildings * dutiesPerBuilding
 
 	chromosome = []
 	for i in range(length):
@@ -75,12 +77,36 @@ def createChromosome():
 	
 	return chromosome
 
+def calculateScore(chromosome):
+	score = 0
+
+	dutyIndex = dutiesPerBuilding
+	bldgIndex = dutyIndex * buildings
+	slotIndex = bldgIndex * dutySlotsPerDay
+	dayIndex = slotIndex * dayCount
+
+	for i in range(len(chromosome)):
+		day = int((i % dayIndex) / slotIndex)
+		slot = (int(i % slotIndex / bldgIndex))
+		bldg = (int(i % bldgIndex / dutyIndex))
+		duty = (int(i % dutyIndex))
+
+		# print(i, chromosome[i])
+		# print(day, slot, bldg, duty)
+		# print(day+1, slot+1, bldg+1, duty+1)
+
+		if chromosome[i] in coreData and coreData[chromosome[i]][day][slot] == "":
+			score += points["free"]
+
+	return score
+
 
 ################################################## Functions ##################################################
 
 ################################################## Run ##################################################
 
-print(createChromosome())
-
+chrom = createChromosome()
+score = calculateScore(chrom)
+print(score)
 
 ################################################## Run ##################################################
